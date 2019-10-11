36f7d829be37a4b0f63d28697859c41f7994d6f4
Iiro Naumanen <iiro.naumanen@aalto.fi>
2019-10-12 00:10:09 +0300

    ir remote

--- main.py
+++ main.py
@@ -5,7 +5,8 @@
 
 from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
 from ev3dev2.sensor.lego import ColorSensor
-from drop_tire import drop_tire
+from drop_tire import drop_dens
+from ir_remote import ir_changed
 
 
 def run_bot():
@@ -13,7 +14,8 @@ def run_bot():
 
     drive_obj = MoveTank(OUTPUT_B, OUTPUT_C)
     cs = ColorSensor()
-    drop_tire()
+    drop_dens()
+    ir_changed()
     end = True
     while end:
         drive_obj.on_for_degrees(SpeedPercent(75), SpeedPercent(75), 1)
