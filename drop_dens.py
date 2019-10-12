b8d2f42e746dcb3532c5ea3016685e54bdd3200e
Iiro Naumanen <iiro.naumanen@aalto.fi>
2019-10-12 15:15:51 +0300

    Setting dens

--- drop_dens.py
+++ drop_dens.py
@@ -1,8 +1,11 @@
 from ev3dev2.motor import MediumMotor, OUTPUT_D, SpeedPercent
+import time
 
 def drop_dens():
     med_motor = MediumMotor(OUTPUT_D)
 
-    med_motor.on_for_degrees(SpeedPercent(70), 90)
+    med_motor.on_for_degrees(SpeedPercent(95), 90)
+    time.sleep(0.1)
+    med_motor.on_for_degrees(SpeedPercent(95), -90)
 
 
