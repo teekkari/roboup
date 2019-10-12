63a3cbf9c5f19a7ca86c4f643debe79844338c20
Lauri Anttila <lauri.3.anttila@aalto.fi>
2019-10-12 20:00:23 +0300

    distance fixes

--- distance_test.py
+++ distance_test.py
@@ -5,4 +5,5 @@ if __name__=="__main__":
 
     while True:
         d = ir.get_distance_cm()
-        print("prox {:.2f} cm, target delta {:.2f} cm".format(d, 20 - d))
+        v = ir.get_ir_value()
+        print("prox {:.2f} cm, ir.value {:.2f} pc".format(d, v))
