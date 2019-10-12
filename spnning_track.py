4ad451db143d12881fb26d1ce41c63393b50ab52
Iiro Naumanen <iiro.naumanen@aalto.fi>
2019-10-12 17:05:10 +0300

    Added printlns

--- spnning_track.py
+++ spnning_track.py
@@ -10,17 +10,20 @@ class Spin():
         self.sensor = ColorSensor()
         
     
-    def go():
+    def go(self):
         self.sensor.mode = 'COL-COLOR'
         try:
+
             self.drive.set_speed(90)
+            print("set speed, 90")
             time = time.time()
 
-
+            
             while time.time() - time < 10:
-                self.drive.turn(20)
-
+                self.drive.turn(-20)
+            print("Turned left for 10s")
             self.drive.set_speed(50)
+            print("Set speed, 50")
             while sensor.color != 6:
                 self.drive.turn(10)
                 time.sleep(2)
@@ -28,7 +31,8 @@ class Spin():
                 self.drive.turn(-20)
                 time.sleep(1)
                 self.drive.stop()
-        except:
+        except Exception as e:
+            print(e)
             self.drive.stop()
 
 if __name__ == "__main__":
