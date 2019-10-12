dc98097a9b753aacc0973025cc1937ad4538b4ad
Iiro Naumanen <iiro.naumanen@aalto.fi>
2019-10-12 13:41:17 +0300

    bug

--- ir_remote.py
+++ ir_remote.py
@@ -7,11 +7,11 @@ class Remote():
         self.ir = InfraredSensor()
         self.ir.mode = 'IR-REMOTE'
         self.drive = Driver()
-        self.ir.on_channel1_beacon = beacon_channel_1_action
-        self.ir.on_channel1_top_left = top_left_channel_1_action
-        self.ir.on_channel1_bottom_left = bot_left_channel_1_action
-        self.ir.on_channel1_top_right = top_right_channel_1_action
-        self.ir.on_channel1_bottom_right = bot_right_channel_1_action
+        self.ir.on_channel1_beacon = self.beacon_channel_1_action
+        self.ir.on_channel1_top_left = self.top_left_channel_1_action
+        self.ir.on_channel1_bottom_left = self.bot_left_channel_1_action
+        self.ir.on_channel1_top_right = self.top_right_channel_1_action
+        self.ir.on_channel1_bottom_right = self.bot_right_channel_1_action
 
     def beacon_channel_1_action(self, state):
         print(self.ir.beacon())
