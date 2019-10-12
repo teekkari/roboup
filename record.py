ee9d5641f318d4993015c432748a6c85f914ad13
Iiro Naumanen <iiro.naumanen@aalto.fi>
2019-10-13 00:48:38 +0300

    Remote and record

---
+++ record.py
@@ -0,0 +1,44 @@
+import pickle
+import time
+from move import Driver
+
+class Record():
+
+    def __init__(self):
+        with open('ghost.pkl', 'rb') as f:
+            self.ghost = pickle.load(f)
+        self.driver = Driver()
+    
+    def play(self):
+        print(len(self.ghost))
+        for move in self.ghost:
+            now = time.time()
+            if move[0] == 'forward':
+                self.driver.move()
+                while True:
+                    if time.time() - now > move[1]:
+                        self.driver.stop()
+                        break
+            if move[0] == 'backward':
+                self.driver.reverse()
+                while True:
+                    if time.time() - now > move[1]:
+                        self.driver.stop()
+                        break
+            if move[0] == 'right':
+                self.driver.turn(100)
+                while True:
+                    if time.time() - now > move[1]:
+                        self.driver.stop()
+                        break
+            if move[0] == 'left':
+                self.driver.turn(-100)
+                while True:
+                    if time.time() - now > move[1]:
+                        self.driver.stop()
+                        break
+        print("Recording ending.")
+
+if __name__ == "__main__":
+    r = Record()
+    r.play()
