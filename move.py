3b77c33cd4175b46c718b99343e67272b7373370
Lauri Anttila <lauri.3.anttila@aalto.fi>
2019-10-12 14:50:33 +0300

    added set_speed to driver

--- move.py
+++ move.py
@@ -4,7 +4,11 @@ from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveStee
 class Driver:
     def __init__(self):
         self.driver = MoveSteering(OUTPUT_B, OUTPUT_C)
-        self.speed = 20
+        self.speed = 40
+
+
+    def set_speed(self, speed):
+        self.speed = max(-100, max(100, speed))
 
     
     def move(self):
