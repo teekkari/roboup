9e7c7e32aa3bc7d918dfdcbb331d7e1820c8fb69
Iiro Naumanen <iiro.naumanen@aalto.fi>
2019-10-13 01:36:09 +0300

    Added args for multiple tracks

--- remote.py
+++ remote.py
@@ -1,5 +1,6 @@
 from ev3dev2.sensor.lego import InfraredSensor
 from move import Driver
+import argparse
 import time
 import pickle
 
@@ -13,21 +14,26 @@ class Remote():
         self.ir.on_channel1_bottom_left = self.bot_left_channel_1_action
         self.ir.on_channel1_top_right = self.top_right_channel_1_action
         self.ir.on_channel1_bottom_right = self.bot_right_channel_1_action
+        self.name = self.parse_args()
         self.ghost = []
         self.now = 0
 
-    def write_file(self, what, how_long):
-        with open('ghost.csv', 'wb', newline='') as f:
-            fields = ['what', 'how_long']
-            writer = csv.DictWriter(f, fieldnames=fields)
-            writer.writeheader()
-            writer.writerow({'what': what, 'how_long': how_long})
+    def parse_args(self):
+        parser = argparse.ArgumentParser(description='Enter filename for recording')
+        parser.add_argument('-n', help='Enter filename for the track')
+        args = parser.args
+        print(args)
+        if not args:
+            raise Exception("Please enter filename after -n argument")
+        return args
+
+
 
     def beacon_channel_1_action(self, state):
         print(self.ir.beacon())
         if state:
             print("Beacon pressed, now stopping")
-            with open('ghost.pkl', 'wb') as f:
+            with open(self.name + 'pkl', 'wb') as f:
                 pickle.dump(self.ghost, f)
 
     def top_left_channel_1_action(self, state):
@@ -36,7 +42,7 @@ class Remote():
             self.now = time.time()
             self.drive.move()
         else:
-            self.ghost.append(('forward', time.time() - self.now))
+            self.ghost.append(('forward', time.time() - self.now + 0.2))
             self.drive.stop()
 
     def bot_left_channel_1_action(self, state):
@@ -45,7 +51,7 @@ class Remote():
             self.now = time.time()
             self.drive.reverse()
         else:
-            self.ghost.append(('backward', time.time() - self.now))
+            self.ghost.append(('backward', time.time() - self.now + 0.2))
             self.drive.stop()
 
     def top_right_channel_1_action(self, state):
@@ -54,7 +60,7 @@ class Remote():
             self.now = time.time()
             self.drive.turn(100)
         else:
-            self.ghost.append(('right', time.time() - self.now))
+            self.ghost.append(('right', time.time() - self.now + 0.2))
             self.drive.stop()
 
     def bot_right_channel_1_action(self, state):
@@ -63,7 +69,7 @@ class Remote():
             self.now = time.time()
             self.drive.turn(-100)
         else:
-            self.ghost.append(('left', time.time() - self.now))
+            self.ghost.append(('left', time.time() - self.now+ 0.2))
             self.drive.stop()
 
     def remote(self):
