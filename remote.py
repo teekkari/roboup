36fe585c8adb089a45b429c585841992e4a8dff1
Iiro Naumanen <iiro.naumanen@aalto.fi>
2019-10-12 22:53:38 +0300

    Old steering

---
+++ remote.py
@@ -0,0 +1,78 @@
+from ev3dev2.sensor.lego import InfraredSensor
+from move import Driver
+import time
+import csv
+
+class Remote():
+    def __init__(self):
+        self.ir = InfraredSensor()
+        self.ir.mode = 'IR-REMOTE'
+        self.drive = Driver()
+        self.ir.on_channel1_beacon = self.beacon_channel_1_action
+        self.ir.on_channel1_top_left = self.top_left_channel_1_action
+        self.ir.on_channel1_bottom_left = self.bot_left_channel_1_action
+        self.ir.on_channel1_top_right = self.top_right_channel_1_action
+        self.ir.on_channel1_bottom_right = self.bot_right_channel_1_action
+        self.ghost = []
+
+    def write_file(self, what, how_long):
+        with open('ghost.csv', 'wb', newline='') as f:
+            fields = ['what', 'how_long']
+            writer = csv.DictWriter(f, fieldnames=fields)
+            writer.writeheader()
+            writer.writerow({'what': what, 'how_long': how_long})
+
+    def beacon_channel_1_action(self, state):
+        print(self.ir.beacon())
+        if state:
+            print("Beacon pressed, now stopping")
+            self.drive.stop()
+
+    def top_left_channel_1_action(self, state):
+        print(self.ir.top_left())
+        if state:
+            now = time.time()
+            self.drive.move()
+            print("Time after forward ", time.time() - now)
+        else:
+            self.drive.stop()
+
+    def bot_left_channel_1_action(self, state):
+        print(self.ir.bottom_left())
+        if state:
+            now = time.time()
+            self.drive.reverse()
+            print("Time after forward ", time.time() - now)
+        else:
+            self.drive.stop()
+
+    def top_right_channel_1_action(self, state):
+        print(self.ir.top_right())
+        if state:
+            now = time.time()
+            self.drive.turn(100)
+            print("Time after forward ", time.time() - now)
+        else:
+            self.drive.stop()
+
+    def bot_right_channel_1_action(self, state):
+        print(self.ir.bottom_right())
+        if state:
+            now = time.time()
+            self.drive.turn(-100)
+            print("Time after forward ", time.time() - now)
+        else:
+            self.drive.stop()
+
+    def remote(self):
+        try:
+            while True:
+                self.ir.process()
+                time.sleep(0.01)
+        except Exception as e:
+            print(e)
+            self.drive.stop()
+
+if __name__ == "__main__":
+    r = Remote()
+    r.remote()
