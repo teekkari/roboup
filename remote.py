66ab15798d32a4b835556dc39194fb90f09d9f39
Lauri Anttila <lauri.3.anttila@aalto.fi>
2019-10-13 09:20:45 +0300

    asd

--- remote.py
+++ remote.py
@@ -9,7 +9,7 @@ class Remote():
         self.ir = InfraredSensor()
         self.ir.mode = 'IR-REMOTE'
         self.drive = Driver()
-        self.drive.set_speed(20)
+        self.drive.set_speed(15)
         self.ir.on_channel1_beacon = self.beacon_channel_1_action
         self.ir.on_channel1_top_left = self.top_left_channel_1_action
         self.ir.on_channel1_bottom_left = self.bot_left_channel_1_action
