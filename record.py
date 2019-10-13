e15269ba9ae317061cb8ecac9171ae9c18a3b5f0
Lauri Anttila <lauri.3.anttila@aalto.fi>
2019-10-13 09:12:26 +0300

    ads

--- record.py
+++ record.py
@@ -10,6 +10,7 @@ class Record():
         with open(self.name + '.pkl', 'rb') as f:
             self.ghost = pickle.load(f)
         self.driver = Driver()
+        self.driver.set_speed(30)
     
 
     def play(self):
